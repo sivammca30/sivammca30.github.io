8eaa2c808f2d771055d1e876ffc1e99e501c79a3
sivammca30 <sivammca30@gmail.com>
2026-07-30 22:15:49 +0530

    Tenkasi Competition Update

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,7 +1,40 @@
 import type { FC } from "react";
-import { UPCOMINGEVENTS } from "../data";
+//import { UPCOMINGEVENTS } from "../data";
 import { MOMENTS } from "../data";
 import type { EventItem } from "../data";
+import UPCOMINGEVENTS from '../assets/json/upcomingevents.json';
+import recentphotos from '../assets/json/recenteventphoto.json';
+import Masonry from './Masonry';
+
+const upcoming = [...UPCOMINGEVENTS]
+  .filter(off => off.status === 'A')
+  .sort((a, b) => a.order - b.order);
+
+const recentimg = [...recentphotos]
+  .filter(off => off.status === 'A')
+  .sort((a, b) => a.order - b.order);
+
+const items = [
+  {
+    id: "1",
+    img: "https://picsum.photos/id/1015/600/900?grayscale",
+    url: "https://example.com/one",
+    height: 400,
+  },
+  {
+    id: "2",
+    img: "https://picsum.photos/id/1011/600/750?grayscale",
+    url: "https://example.com/two",
+    height: 250,
+  },
+  {
+    id: "3",
+    img: "https://picsum.photos/id/1020/600/800?grayscale",
+    url: "https://example.com/three",
+    height: 600,
+  },
+  // ... more items
+];
 
 const Events: FC = () => (
   <>
@@ -13,29 +46,33 @@ const Events: FC = () => (
       </div>
     </section>
 
-    <section className="section">
+    <section className="section-small">
       <div className="container">
         <div className="event-grid">
-          {UPCOMINGEVENTS.map((ev: EventItem) => {
-            // const formatted: string = new Date(ev.date).toLocaleDateString("en-IN", {
-            //   year: "numeric",
-            //   month: "long",
-            //   day: "numeric",
-            // });
+          {upcoming.map((ev) => {
+            const formatted: string = new Date(ev.date).toLocaleDateString("en-IN", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            });
             return (
               <div key={ev.id} className="event-card">
                 <div className="event-card-placeholder">
                   <span>WTSF - {ev.title}</span>
+
                 </div>
                 <div className="event-card-body">
                   {/* <div className="blog-card-date">{formatted}</div>
                   <span className="blog-card-cat">{ev.category}</span>
                   <div className="blog-card-title">{ev.title}</div>
                   <p className="blog-card-desc">{ev.desc}</p> */}
-                   <img className="event-card-image"
-                      src={ev.img}
-                      alt={ev.title}
-                    />
+                  <img className="event-card-image"
+                    src={ev.img}
+                    alt={ev.title}
+                  />
+                </div>
+                <div className="event-card-footer">
+                  <div className="blog-card-date">{formatted}</div>
                 </div>
               </div>
             );
@@ -44,7 +81,42 @@ const Events: FC = () => (
       </div>
     </section>
 
-     <section className="section section-dark">
+
+
+    <div className="event-section-small event-section-dark">
+      <div className="event-container"> {/* Changed from 'container' to match your global max-width style */}
+
+        <div className="event-section-title">
+          <h2>RECENT EVENTS</h2>
+          <span>WTSF District Competition - Tenkasi - 2026</span>
+          <p>
+            WTSF Organised 4th District level Silambam Open Championship in Sri Durga Vani Sports Academy, Melapuliyur, Tenkasi on 26 August 2026.
+            Over 300 Students participated in this event. Single Stick, Double Stick, Chedikuchi, Surrul Vaal, Vel Kambu, Maan Kombu and Thodumurai Events were conducted by our WTSF Refrees/Judges.
+          </p>
+        </div>
+
+        {/* Masonry image layout wrapper */}
+
+        <Masonry
+          items={recentimg}
+          ease="power3.out"
+          duration={0.6}
+          stagger={0.05}
+          animateFrom="bottom"
+          scaleOnHover
+          hoverScale={0.95}
+          blurToFocus
+          colorShiftOnHover={false}
+        />
+
+
+
+      </div>
+    </div>
+
+
+
+    {/* <section className="section section-dark">
       <div className="container">
         <div className="section-title"><h2>RECENT EVENTS</h2></div>
         <div className="blog-grid">
@@ -73,7 +145,7 @@ const Events: FC = () => (
         </div>
        
       </div>
-    </section>
+    </section> */}
   </>
 );
 
